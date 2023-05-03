8499cae2177bf9d34d7253d74ae3cd1cc2a99b2b
Masum <masumt999.com>
2023-05-03 12:02:20 +0600

    about us section added

--- src/pages/Section/AboutSection/AboutSection.jsx
+++ src/pages/Section/AboutSection/AboutSection.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-
-// const AboutSection = () => {
-//     return (
-//         <section className="py-10">
-//             <div className="container mx-auto">
-//                 <h2 className="text-3xl font-bold text-center mb-4">About Us</h2>
-//                 <div className='d-flex'>
-//                     <div>
-//                         <p className="text-lg mb-8">We are a group of passionate foodies who love to cook, bake, and experiment in the kitchen. Our mission is to share our love of food with the world and inspire others to try new recipes and techniques.</p>
-//                         <p className="text-lg mb-8">Our website features a variety of recipes from around the world, as well as tips and tricks for cooking and baking. We believe that food brings people together and that everyone should have access to delicious and healthy meals.</p>
-//                         <p className="text-lg mb-8">Thank you for visiting our website and we hope you find inspiration and joy in cooking!</p>
-//                     </div>
-//                     <div>
-//                         <img src='https://img.freepik.com/free-photo/chef-holding-vegetables_1368-11929.jpg?w=996&t=st=1683052259~exp=1683052859~hmac=3fa4fad89da5d1f4702c6550468bdbb3ff251b7ef15608d3b10796b36d4cfeca'></img>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default AboutSection;
-
 import React from 'react';
 
 
--- src/pages/Section/ContactSection/ContactSection.jsx
+++ src/pages/Section/ContactSection/ContactSection.jsx
@@ -6,14 +6,14 @@ const ContactSection = () => {
             <div className="grid grid-cols-2 gap-10 py-16 container mx-auto items-center">
                 <div>
 
-                    {/* <div className="grid grid-cols-3 gap-4 ">
+                    <div className="grid grid-cols-3 gap-4 ">
                         <img src="https://img.freepik.com/free-photo/top-view-food-ingredients_23-2148834755.jpg?w=1380&t=st=1683092892~exp=1683093492~hmac=8f62c90624dd79ac1b36f85b58c561f678a96e03a81dc14ce34556343cb38255" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
                         <img src="https://img.freepik.com/free-photo/top-view-delicious-salad-with-ingredients-dark-surface_140725-75212.jpg?w=1380&t=st=1683092919~exp=1683093519~hmac=a7d8d758c8270bbed227ef4dcbd99f2505a306a999c9732ce8bac8b7452911e1" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
                         <img src="https://img.freepik.com/free-photo/top-view-delicious-meat-soup-with-greens-potatoes-dark-desk_140725-76538.jpg?w=1380&t=st=1683092933~exp=1683093533~hmac=0e956c80d6ce2c913495cf06ce19ea5dad0de90d8f33858802e7075355816b70" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
                         <img src="https://img.freepik.com/free-photo/top-view-food-ingredients-with-notebook-pumpkin_23-2148834716.jpg?w=740&t=st=1683092953~exp=1683093553~hmac=76e77732b9f86cf533dd1d89231dc692a7adcbdd7e72c62e93c15835cc17bd9d" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
                         <img src="https://img.freepik.com/free-photo/top-view-chicken-salad-with-vegetables-dark-grey-table-diet-health-salad_140725-73203.jpg?t=st=1683092882~exp=1683093482~hmac=496e1d832fb93c60c7c756acc0841f67888388c98b6dffa22802f2c78c9eccfb" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
                         <img src="https://img.freepik.com/free-photo/top-view-greens-seasonings-with-sliced-onions-notepad-dark-desk-salad-food-meal-vegetable-snack_140725-40291.jpg?w=1380&t=st=1683092986~exp=1683093586~hmac=d2591c05691d5cce8e12dec41e163526a5c7f2db4c24742114438b9e2ff0ba07" alt="placeholder" className="w-full h-full object-cover rounded-lg" />
-                    </div> */}
+                    </div>
                 </div>
                 <div>
                     <h2 className="text-3xl font-bold mb-8">Contact Us</h2>
